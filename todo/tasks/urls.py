2ca5df80e428506fd30a06d266c4c9776a8522dc
Michael Magdosa <michael.magdosa.dev@gmail.com>
2025-09-15 16:20:29 +0800

    delete method added

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name=""),
+    path('update-task/<str:pk>/',views.updateTask, name="update-task"),
+    path('delete-task/<str:pk>/', views.deleteTask, name='delete-task'), 
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -18,14 +18,45 @@ def index(request):
             form.save()
         return redirect('/')
         
-
-
-
-
     context = {'tasks': tasks,
                'TaskForm': form}
 
     return render(request, 'tasks/tasks.html', context=context)
+
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    
+    form = TaskForm(instance=task)
+
+    if request.method =='POST':
+        form = TaskForm(request.POST, instance=task)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+
+    context = {'TaskForm': form}
+
+    return render(request, 'tasks/update-task.html', context=context)
+
+
+
+def deleteTask(request, pk):
+
+    task = Task.objects.get(id=pk)
+
+    if request.METHOD == 'POST':
+        task.delete()
+        return redirect('/')
+    context = {'task':task}
+
+    return render(request, 'tasks/delete-task.html', context=context)
+
+
+
 
 
 
