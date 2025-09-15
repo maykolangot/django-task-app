a1523cdf8f692f85ec68cf24912edd352d3682b3
Michael Magdosa <michael.magdosa.dev@gmail.com>
2025-09-15 11:00:43 +0800

    forms created

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 
 
 from .models import Task
+
+from . forms import TaskForm
 # Create your views here.
 
 def index(request):
 
+    form = TaskForm()
+
     tasks = Task.objects.all()
 
-    context = {'tasks': tasks}
+    context = {'tasks': tasks,
+               'TaskForm': form}
 
-    return render(request, 'tasks/tasks.html', context=tasks)
+    return render(request, 'tasks/tasks.html', context=context)
 
 
 
